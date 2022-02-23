69d5c025329faf83434c44feaa5b0f7e8fe46c8c
Guilherme Teruaki <guilherme.teruaki@gmail.com>
2022-02-23 12:45:27 +0000

    homework 2 working

--- src/homeworkswdevclass/HomeworkSWDevClass.java
+++ src/homeworkswdevclass/HomeworkSWDevClass.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package homeworkswdevclass;
 
 
@@ -18,24 +14,39 @@ public class HomeworkSWDevClass {
 
     public static void main(String[] args) {
        
-        Scanner scanInput = new Scanner(System.in);
+        Scanner scanInput = new Scanner(System.in); //initiates the scanner
         
-        boolean endLoop = false;
+        boolean endLoop = false; //manages the loop
         double userGrade;
-        do {
-            System.out.println("Please provide your grade, use values 0 - 100");
-            try{
-                userGrade = scanInput.nextDouble();
-                System.out.println(userGrade);
-                endLoop = true;
+        do { //a loop to run the question again if the input is invalid
+            System.out.println("Please provide your grade, use values 0 - 100"); //ask the question
+            try{ //try/catch for scanner 
+                userGrade = scanInput.nextDouble(); //gets the next double number inputed
+                if(userGrade<0 || userGrade>100){ //checks if the grade is a valid number between 0-100
+                    System.out.println("That is not a valid grade");
+                    main(args); /*This reruns the program in case a invalid grade is used,
+                    is it better to call the main metod again or to use do/whiles ?*/
+                } else{
+                    double proportionalGrade = (userGrade/100)*25; //manipulates the variable as needed
+                    System.out.printf("Your grade was %.1f%% that is equivalent to %.2f%% "
+                                     + "of your total grade.\n", userGrade, proportionalGrade);
+                }
+                endLoop = true; //set the loop to end since inputs were valid.
                       
             }catch (InputMismatchException e) {
-                System.out.println("that´s not a number, try again");
-                
-            }
-        } while(endLoop==false);
+                System.out.println("that´s not a number, try again"); //catches any input that it's  not a number
+                scanInput.nextLine(); // without it the loob will bug and enter a infinite loop
+                        }
+        } while(endLoop==false); //condition to finish the while loop.
     
      
+        
+        
+        
+        
+        
+        
+        
 //        System.out.println("Please tell me your age");
 //        try{
 //           int age=scanInput.nextInt();
