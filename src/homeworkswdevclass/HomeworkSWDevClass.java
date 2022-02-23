638abb92662cc8caa32b83d3b7fe19c8faad95dc
Guilherme Teruaki <guilherme.teruaki@gmail.com>
2022-02-23 13:03:30 +0000

    homework 2 working

--- src/homeworkswdevclass/HomeworkSWDevClass.java
+++ src/homeworkswdevclass/HomeworkSWDevClass.java
@@ -29,8 +29,8 @@ public class HomeworkSWDevClass {
                     is it better to call the main metod again or to use do/whiles ?*/
                 } else{
                     double proportionalGrade = (userGrade/100)*25; //manipulates the variable as needed
-                    System.out.printf("Your grade was %.1f%% that is equivalent to %.2f%% "
-                                     + "of your total grade.\n", userGrade, proportionalGrade);
+                    System.out.printf("Your grade was %.1f%% that is equivalent to "
+                                     + "%.2f%% of your total grade.", userGrade, proportionalGrade);
                 }
                 endLoop = true; //set the loop to end since inputs were valid.
                       
