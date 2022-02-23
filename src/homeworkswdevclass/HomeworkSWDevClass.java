bbf70cedeb6467877741633a74994265fd1a1807
Guilherme Teruaki <52307826+guilhermeteruaki@users.noreply.github.com>
2022-02-23 10:13:54 +0000

    Add files via upload

---
+++ src/homeworkswdevclass/HomeworkSWDevClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package homeworkswdevclass;
+
+
+/**
+ *
+ * @author guilherme teruaki CCT No 2022047
+ */
+public class HomeworkSWDevClass {
+
+
+    public static void main(String[] args) {
+        
+    testAnswer = new homework2()
+}
